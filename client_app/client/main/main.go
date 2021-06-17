52732c7659d765b1b9526c0e84ce93a37ad3e056
LibenHailu <libenhailu04@gmail.com>
2021-06-17 21:29:30 +0300

    final commit

--- client_app/client/main/main.go
+++ client_app/client/main/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-
 	filesource "github.com/LibenHailu/grpc_file_stream/file_stream/file_source"
 	"github.com/LibenHailu/peer_to_peer_file_share/peer/client_app/client"
 	"github.com/gin-gonic/gin"
+	"github.com/LibenHailu/grpc_file_stream/file_stream/file_client"
+	// mainpb "github.com/LibenHailu/grpc_file_stream/file_stream/filepb"
 )
 
 func main() {
@@ -25,24 +25,14 @@ func main() {
 			"server":         fmt.Sprintf("served by %s", *server),
 			"result":         fmt.Sprintf("file %s saved successfuly", name),
 		})
+		return
 	})
 
-	g.POST("/file/upload", func(ctx *gin.Context) {
-		// single file
-		file, _ := ctx.FormFile("file")
-		log.Println(file.Filename)
+	g.POST("file/upload", func(ctx *gin.Context) {
+		
+		fileclient.UploadFileClient(ctx)
 
-		// cc := fileclient.Connect()
-		// c := mainpb.NewFileServiceClient(cc)
-		// fmt.Println(c)
-
-		fmt.Println(file)
-		// fileclient.UploadFile(c,)
-
-		// Upload the file to specific dst.
-		// c.SaveUploadedFile(file, dst)
-
-		// c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
 	})
-	g.Run(":8081")
+
+	g.Run(":8082")
 }
